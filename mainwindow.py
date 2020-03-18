d6e7198e23b5d1be6afb6678c2bf04117965dda5
vikilog <svikasswami03@gmail.com>
2020-03-18 11:58:48 +0530

    Button added

--- mainwindow.py
+++ mainwindow.py
@@ -1,11 +1,20 @@
-from PyQt5.QtWidgets import QMainWindow,QApplication
+from PyQt5.QtWidgets import QMainWindow,QApplication,QPushButton,QVBoxLayout,QDialog
 import sys
 
-class Window(QMainWindow):
+class Window(QDialog):
     def __init__(self,):
-        QMainWindow.__init__(self)
-
+        QDialog.__init__(self)
+        self.setMinimumSize(500,400)
+        vbox=QVBoxLayout()
+        button=QPushButton("Click Me")
+        button.clicked.connect(self.click)
+        vbox.addWidget(button)
+        self.setLayout(vbox)
+        
         self.show()
+    def click(self):
+        print("clicked")
+    
 
 app=QApplication(sys.argv)
 window=Window()
